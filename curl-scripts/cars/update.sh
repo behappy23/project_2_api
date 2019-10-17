8bdb85d42ce951404c6b5a531f8398c4ca38356a
behappy23 <beharry.jason30@gmail.com>
2019-10-17 15:25:11 -0400

    created curl scripts

---
+++ curl-scripts/cars/update.sh
@@ -0,0 +1,17 @@
+# Ex: TOKEN=tokengoeshere ID=idgoeshere TEXT=textgoeshere sh curl-scripts/examples/update.sh
+
+curl "http://localhost:4741/cars/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "car": {
+      "year": "'"${YEAR}"'"
+      "make": "'"${MAKE}"'"
+      "model": "'"${MODEL}"'"
+      "vin": "'"${VIN}"'"
+    }
+  }'
+
+  echo
